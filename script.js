91198b545bee733bdb1c77d296693c3dcd47df13
Anthony <akingori371@gmail.com>
2026-08-04 09:34:29 +0300

    Add "Back to Home" button in man.html and women.html; update script.js for navigation

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 const womenBtn = document.getElementById(`exploreWomenBtn`);
 const menBtn = document.getElementById(`exploreMenBtn`);
-
+const homeBtn = document.getElementById(`home`);
 menBtn.addEventListener(`click`, () => {
     window.location.href = "man.html"
 });
 womenBtn.addEventListener(`click`, () => {
     window.location.href = "women.html"
+});
+homeBtn.addEventListener(`click`, () => {
+    window.location.href = "index.html"
 });
