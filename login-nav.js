ce68447ed08bd44d3c2e22508f7c73f47a269e15
Anthony <akingori371@gmail.com>
2026-08-04 15:12:10 +0300

    add a login page

---
+++ login-nav.js
@@ -0,0 +1,12 @@
+
+const loginButton = document.getElementById('login');
+
+loginButton.addEventListener('click', () => {
+  window.location.href = 'login.html';
+});
+
+function signInUser(){
+    windows.location.href = 'login.html';
+}
+
+if ()
